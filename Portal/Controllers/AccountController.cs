e37b7492762cf1a544229e89c4cf592d7ce187c0
JackySong <songzheyu_works@163.com>
2020-08-07 01:58:50 +0800

    Add cookie

--- Portal/Controllers/AccountController.cs
+++ Portal/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Portal.DB;
@@ -25,13 +26,22 @@ namespace Portal.Controllers
         public IActionResult Index(String username, String password, String appkey)
         {
             ViewData["appkey"] = appkey;
+            Account selectedAccount = null;
+            Application selectedApp = null;
             bool loginSuccess = false;
+            HttpContext.Request.Cookies.TryGetValue("Login", out String loginUser);
             var accounts = from ac in this._accountContext.Account select ac;
             var applications = from app in this._applicationContext.Application select app;
-            
-            if (!String.IsNullOrEmpty(username) && !String.IsNullOrEmpty(password))
+
+            if (loginUser != null)
             {
-                Account selectedAccount = accounts.Where(ac => ac.Username == username).FirstOrDefault();
+                selectedAccount = accounts.Where(ac => ac.Username == username).FirstOrDefault();
+                ViewData["status"] = "Login Success";
+                loginSuccess = true;
+            }
+            else if (!String.IsNullOrEmpty(username) && !String.IsNullOrEmpty(password))
+            {
+                selectedAccount = accounts.Where(ac => ac.Username == username).FirstOrDefault();
                 if (selectedAccount == null)
                 {
                     ViewData["status"] = "No User";
@@ -41,7 +51,11 @@ namespace Portal.Controllers
                     if (selectedAccount.Password == password)
                     {
                         ViewData["status"] = "Login Success";
-                        loginSuccess = true; // TODO: Change URL here
+                        HttpContext.Response.Cookies.Append("Login", selectedAccount.Username, new CookieOptions
+                        {
+                            Expires = DateTime.Now.AddMinutes(30)
+                        });
+                        loginSuccess = true;
                     }
                     else
                     {
@@ -62,7 +76,7 @@ namespace Portal.Controllers
                 }
                 else
                 {
-                    Application selectedApp = applications.Where(app => app.Appkey == appkey).FirstOrDefault();
+                    selectedApp = applications.Where(app => app.Appkey == appkey).FirstOrDefault();
                     try
                     {
                         return Redirect(selectedApp.Url);
