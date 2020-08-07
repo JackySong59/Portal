87896ee2f17c2b30b7f81fd509da48c5bcde9c07
JackySong59 <1055335350@qq.com>
2020-08-07 10:25:51 +0800

    Add Login Verification Api

--- Portal/Controllers/ApiController.cs
+++ Portal/Controllers/ApiController.cs
@@ -1,20 +1,72 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Portal.DB;
+using System.Linq;
+using Portal.Models;
 
 namespace Portal.Controllers
 {
     public class ApiController : Controller
     {
-        // GET
-        public String Index()
-        {
-            return JsonConvert.SerializeObject(new {test = "test"});
-        }
+        private readonly TicketContext _ticketContext;
 
-        public String Login(String username, String password)
+        public ApiController(
+            TicketContext ticketContext
+        )
         {
-            return null;
+            this._ticketContext = ticketContext;
+        }
+        
+        // GET
+        public String Index(String appkey, String ticket)
+        {
+            var tickets = from t in this._ticketContext.Ticket select t;
+            if (!String.IsNullOrEmpty(appkey) && !String.IsNullOrEmpty(ticket))
+            {
+                Ticket selectedTicket = tickets.Where(t => t.Number == ticket).FirstOrDefault();
+                if (selectedTicket.Appkey == appkey)
+                {
+                    try
+                    {
+                        return JsonConvert.SerializeObject(new
+                        {
+                            username = selectedTicket.Username
+                        });
+                    }
+                    catch (NullReferenceException e)
+                    {
+                        System.Console.WriteLine(e.Message);
+                    }
+                }
+            }
+
+            return "No Ticket";
+        }
+        
+        public String LoginVerification(String appkey, String ticket)
+        {
+            var tickets = from t in this._ticketContext.Ticket select t;
+            if (!String.IsNullOrEmpty(appkey) && !String.IsNullOrEmpty(ticket))
+            {
+                Ticket selectedTicket = tickets.Where(t => t.Number == ticket).FirstOrDefault();
+                if (selectedTicket.Appkey == appkey)
+                {
+                    try
+                    {
+                        return JsonConvert.SerializeObject(new
+                        {
+                            username = selectedTicket.Username
+                        });
+                    }
+                    catch (NullReferenceException e)
+                    {
+                        System.Console.WriteLine(e.Message);
+                    }
+                }
+            }
+
+            return "No Ticket";
         }
     }
 }
